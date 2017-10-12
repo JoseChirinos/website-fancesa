0b396e3bad08aa0f65fca581094bf933f283ec2d
JoseChirinos <josealexcba@gmail.com>
2017-10-12 12:04:34 -0400

    Segunda Version del sitio web fancesa

--- js/index.js
+++ js/index.js
@@ -1,20 +1,77 @@
 /* code © 2017 - Jose Chirinos */
+/* var global */
+var consulta = window.matchMedia('(max-width: 768px)');
+function scrollToTop(scrollDuration) {
+    var cosParameter = window.scrollY / 2,
+        scrollCount = 0,
+        oldTimestamp = performance.now();
+    function step (newTimestamp) {
+        scrollCount += Math.PI / (scrollDuration / (newTimestamp - oldTimestamp));
+        if (scrollCount >= Math.PI) window.scrollTo(0, 0);
+        if (window.scrollY === 0) return;
+        window.scrollTo(0, Math.round(cosParameter + cosParameter * Math.cos(scrollCount)));
+        oldTimestamp = newTimestamp;
+        window.requestAnimationFrame(step);
+    }
+    window.requestAnimationFrame(step);
+}
 /* Pollyfill for ie*/
-var common = {
-    getChildren: function(element) {
-        var childNodes = element.childNodes,
-            children = [],
-            i = childNodes.length;
+if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function(callback, thisArg) {
 
-        while (i--) {
-            if (childNodes[i].nodeType == 1) {
-                children.unshift(childNodes[i]);
-            }
+        var T, k;
+
+        if (this == null) {
+        throw new TypeError(' this is null or not defined');
         }
 
-        return children;
-    }
-};
+        // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
+        var O = Object(this);
+
+        // 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
+        // 3. Let len be ToUint32(lenValue).
+        var len = O.length >>> 0;
+
+        // 4. If IsCallable(callback) is false, throw a TypeError exception.
+        // See: http://es5.github.com/#x9.11
+        if (typeof callback !== "function") {
+        throw new TypeError(callback + ' is not a function');
+        }
+
+        // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+        if (arguments.length > 1) {
+        T = thisArg;
+        }
+
+        // 6. Let k be 0
+        k = 0;
+
+        // 7. Repeat, while k < len
+        while (k < len) {
+
+        var kValue;
+
+        // a. Let Pk be ToString(k).
+        //   This is implicit for LHS operands of the in operator
+        // b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
+        //   This step can be combined with c
+        // c. If kPresent is true, then
+        if (k in O) {
+
+            // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
+            kValue = O[k];
+
+            // ii. Call the Call internal method of callback with T as the this value and
+            // argument list containing kValue, k, and O.
+            callback.call(T, kValue, k, O);
+        }
+        // d. Increase k by 1.
+        k++;
+        }
+        // 8. return undefined
+    };
+}
+/* Menu Bar */
 var menuCommon = {
     inside: null,
     bar: null,
@@ -28,12 +85,7 @@ var menuCommon = {
         /* add event hover */
         var self = this;
         /* order sub-menus */
-        [].forEach.call(this.ulElem.children,function(i){
-            var a = i.children[0];
-            if(typeof i.children[1]!== 'undefined'){
-                i.children[1].style.left = a.offsetLeft+'px';
-            }
-        });
+        this.reOrderSub();
         [].forEach.call(this.ulElem.children,function(i){
             i.onmouseenter = function() {
                 var a = i.children[0];
@@ -45,73 +97,26 @@ var menuCommon = {
             self.initMenu();
         };
     },
+    reOrderSub: function(){
+        console.log(typeof this.ulElem.children);
+        [].forEach.call(this.ulElem.children,function(i){
+            var a = i.children[0];
+            if(typeof i.children[1]!== 'undefined'){
+                i.children[1].style.left = a.offsetLeft+'px';
+            }
+        });
+    },
     initMenu: function(){
         var on = document.querySelector('.on').children[0];
-        this.bar.style.width = on.offsetWidth+'px';
-        this.bar.style.transform = "translate3d("+ (on.offsetLeft - 15) +'px,'+' 0px, 0px)';
+        if(this.bar!==null){
+            this.bar.style.width = on.offsetWidth+'px';
+            this.bar.style.transform = "translate3d("+ (on.offsetLeft - 15) +'px,'+' 0px, 0px)';
+        }
     },
     isTop:true,
 };
+/*functions for maps by Jose Chirinos*/
 (function(){
-    if (!Array.prototype.forEach) {
-
-        Array.prototype.forEach = function(callback, thisArg) {
-
-            var T, k;
-
-            if (this == null) {
-            throw new TypeError(' this is null or not defined');
-            }
-
-            // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
-            var O = Object(this);
-
-            // 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
-            // 3. Let len be ToUint32(lenValue).
-            var len = O.length >>> 0;
-
-            // 4. If IsCallable(callback) is false, throw a TypeError exception.
-            // See: http://es5.github.com/#x9.11
-            if (typeof callback !== "function") {
-            throw new TypeError(callback + ' is not a function');
-            }
-
-            // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
-            if (arguments.length > 1) {
-            T = thisArg;
-            }
-
-            // 6. Let k be 0
-            k = 0;
-
-            // 7. Repeat, while k < len
-            while (k < len) {
-
-            var kValue;
-
-            // a. Let Pk be ToString(k).
-            //   This is implicit for LHS operands of the in operator
-            // b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
-            //   This step can be combined with c
-            // c. If kPresent is true, then
-            if (k in O) {
-
-                // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
-                kValue = O[k];
-
-                // ii. Call the Call internal method of callback with T as the this value and
-                // argument list containing kValue, k, and O.
-                callback.call(T, kValue, k, O);
-            }
-            // d. Increase k by 1.
-            k++;
-            }
-            // 8. return undefined
-        };
-        }
-})(window);
-(function(){
-    /*functions for maps by Jose Chirinos*/
     var deps = document.querySelector('#deps');
     var urls = [];
     /* valores generales */
@@ -135,93 +140,87 @@ var menuCommon = {
         on: function(){
             this.elem.onmouseenter = function(e){
                 e.preventDefault();
-                console.log('entra');
+                /*console.log('entra');*/
                 this.setAttribute('class','');
                 document.querySelector('.ch').setAttribute('class','land ch');
             }
             this.elem.onmouseleave = function(e){
                 e.preventDefault();
-                console.log('sale');
+                /*console.log('sale');*/
                 this.setAttribute('class','dep-def');
                 document.querySelector('.ch').setAttribute('class','land ch dhover');
             }
+            document.querySelector('.ch').setAttribute('class','land ch dhover');
         }
     };
     def.on();
 })(window);
+/*event scroll by Jose Chirinos*/
 (function(env){
-    /*functions for menu navigation by Jose Chirinos*/
-    env.onload = function(){
-        menuCommon.init();
-        menuCommon.initMenu();
-    }
-})(window);
-(function(env){
-    /*event scroll by Jose Chirinos*/
+    /* iniciar menu */
+    menuCommon.init();
+    menuCommon.initMenu();
+
     var header = document.querySelector('.header').offsetTop - 50;
     var sa = document.querySelector('.sNa').offsetTop - 50;
     var sb = document.querySelector('.sNb').offsetTop - 50;
     var sc = document.querySelector('.sNb').offsetTop - 50;
     var sd = document.querySelector('.sNd').offsetTop - 50;
     var menu = document.querySelector('.menu');
+    var burger = document.querySelector('#menu-icon-wrapper');
+    var top = document.querySelector('.upbottom');
 
     /*var for animate pack*/
     var packHeader = document.querySelector('.pack-fancesa-top');
-    var pack = document.querySelector('.pack-fancesa');
     /*end*/
 
     env.onscroll = function(){
         if(window.scrollY>header && window.scrollY<sa){
             var sh = 50 - window.scrollY*0.5;
             /*console.log(sh);*/
-            packHeader.style.top = sh+'px';          
+            /*console.log(packHeader.children[0]);*/
+            packHeader.children[0].style.transform = 'translateY('+sh+'px)';
         }
         if(window.scrollY > sa-100){
             if(menuCommon.isTop){
+                top.style.display = 'block';
+                top.setAttribute('class','upbottom animated rotateIn');
                 menu.style.position = 'fixed';
-                menu.children[0].style.display = 'none';
+                if(!consulta.matches){
+                    menu.children[0].style.display = 'none';
+                }
+                else{
+                    burger.style.position = 'fixed';
+                }
                 menu.querySelector('#logo-nav').style.display = 'inline-block';
                 menu.style.background = 'rgba(0, 0, 0, 0.9)';
                 [].forEach.call(document.querySelectorAll('.menu a'),function(i){
                     i.style.color = '#fff';
                 });
                 //document.querySelector('.bar').style.background = '#fff';
+                menuCommon.reOrderSub();
                 menuCommon.initMenu();
                 menuCommon.isTop = false;
             }
         }
         else{
             if(!menuCommon.isTop){
+                top.setAttribute('class','upbottom animated bounceOut');
                 menu.style.position = 'absolute';
                 menu.children[0].style.display = 'block';
+                if(consulta.matches){
+                    burger.style.position = 'absolute';
+                }
                 menu.querySelector('#logo-nav').style.display = 'none';
                 menu.style.background = 'transparent';
                 [].forEach.call(document.querySelectorAll('.menu a'),function(i){
                     i.style.color = '#FFFFFF';
                 });
                 //document.querySelector('.bar').style.background = '#be0411';
+                menuCommon.reOrderSub();
                 menuCommon.initMenu();
                 menuCommon.isTop = true;
             }
-        }
-        if(window.scrollY > (sb -500) && window.scrollY < (sd)){
-                var sss = 50 - (window.scrollY - sb)*0.4;
-                /*
-                if(sss>=0 || sss<-100){
-                    if(sss<-100){
-                        pack.style.top = sss+100+'px';
-                    }
-                    else{
-                        pack.style.top = sss+'px';
-                    }
-                }
-                */
-                if(sss>=0){
-                    pack.style.top = sss+'px';
-                }
-                else{
-                    pack.style.right = 100 - sss*0.2 + 'px';
-                }
         }
         
     }
